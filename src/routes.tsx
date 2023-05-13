49c62906103f9b83d08013010f8aa083cd3f9347
Natanael de Paulo <natanaeldepaulo123@gmail.com>
2023-05-12 22:18:39 -0300

    routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,14 @@
-import { createBrowserRouter } from "react-router-dom";
-import { SignIn } from "./pages/SignIn";
+import { createBrowserRouter } from 'react-router-dom'
+import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <SignIn />
-    }
-  ])
+  {
+    path: '/',
+    element: <SignIn />
+  },
+  {
+    path: '/signup',
+    element: <SignUp />
+  }
+])
